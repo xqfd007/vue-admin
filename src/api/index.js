f336ef98c8fc0d572c8d5d83e250e59004d49c11
xiaqingfeng <xqfd007@163.com>
2022-02-21 17:00:56 +0800

    11

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,49 @@
-import http from "@/utils/httpRequest";
-import { getToken } from "@/utils/auth";
-import Qs from "qs";
+import http from '@/utils/httpRequest'
+import { getToken } from '@/utils/auth'
+import Qs from 'qs'
 /* 登录 */
 export function login(user) {
-  return http.get("/dev-api/login?user=" + user);
+  return http.get('/dev-api/login?user=' + user)
 }
 /* 菜单列表 */
 export function fetchPermission() {
-  return http.get("/dev-api/permission?user=" + getToken());
+  return http.get('/dev-api/permission?user=' + getToken())
 }
 /* 退出登录 */
 export function logout() {
-  return http.get("/dev-api/logout");
+  return http.get('/dev-api/logout')
 }
 /* 新增目录菜单 */
 export function addmenu(data) {
-  return http.post("dev-api/students/new", data);
+  http.post('dev-api/students/new', data)
+
+  // http({
+  //   method: 'post',
+  //   url: 'dev-api/students/new',
+  //   data: {
+  //     dad: 'dsds',
+  //     drt: 'dsdsaa',
+  //   },
+  //   params: {
+  //     name: 'w',
+  //     age: 18,
+  //   },
+  // })
+
+  // http({
+  //   method: 'post',
+  //   url: 'dev-api/students/new',
+  //   data,
+  // })
+
+  // http.post('dev-api/students/new', {
+  //   name: 'dwe',
+  //   age: 13,
+  // })
+  // http.post('dev-api/students/new', {
+  //   params: {
+  //     naem: 'ds',
+  //     age: 72,
+  //   },
+  // })
 }
